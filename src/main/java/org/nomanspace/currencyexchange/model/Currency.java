52e1619319635121cfceec8f7c00053306fbc89a
Nomanspace <Warhammerd@gmail.com>
2025-02-16 15:00:26 +0300

    Add model classes: Currency and EcvhangeRate.

---
+++ src/main/java/org/nomanspace/currencyexchange/model/Currency.java
@@ -0,0 +1,51 @@
+package org.nomanspace.currencyexchange.model;
+
+public class Currency {
+    private Integer id;
+    private String code;
+    private String fullName;
+    private String sign;
+
+    public Currency() {
+
+    }
+
+    public Currency(Integer id, String code, String fullName, String sign) {
+        this.id = id;
+        this.code = code;
+        this.fullName = fullName;
+        this.sign = sign;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getSign() {
+        return sign;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setSign(String sign) {
+        this.sign = sign;
+    }
+}
---
+++ src/main/java/org/nomanspace/currencyexchange/model/ExchangeRate.java
@@ -0,0 +1,53 @@
+package org.nomanspace.currencyexchange.model;
+
+import java.math.BigDecimal;
+
+public class ExchangeRate {
+    private Integer id;
+    private Currency baseCurrency;
+    private Currency targetCurrency;
+    private BigDecimal rate;
+
+    public ExchangeRate() {
+
+    }
+
+    public ExchangeRate(Integer id, Currency baseCurrency, Currency targetCurrency, BigDecimal rate) {
+        this.id = id;
+        this.baseCurrency = baseCurrency;
+        this.targetCurrency = targetCurrency;
+        this.rate = rate;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Currency getBaseCurrency() {
+        return baseCurrency;
+    }
+
+    public void setBaseCurrency(Currency baseCurrency) {
+        this.baseCurrency = baseCurrency;
+    }
+
+    public Currency getTargetCurrency() {
+        return targetCurrency;
+    }
+
+    public void setTargetCurrency(Currency targetCurrency) {
+        this.targetCurrency = targetCurrency;
+    }
+
+    public BigDecimal getRate() {
+        return rate;
+    }
+
+    public void setRate(BigDecimal rate) {
+        this.rate = rate;
+    }
+}
